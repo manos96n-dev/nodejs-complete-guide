111093690eb883b957b0a1e5ef0db41d3ca80849
manos96n-dev <manos96n@hotmail.com>
2023-08-10 21:20:38 +0300

    Simple express example

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// Middleware function
+app.use((req, res, next) => {
+  console.log('In the middleware');
+  next(); // Allows the request to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);
