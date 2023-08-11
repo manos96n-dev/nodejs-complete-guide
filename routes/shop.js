ee56528fce787ab5e01d60bbc88be441e528cce1
manos96n-dev <manos96n@hotmail.com>
2023-08-12 00:53:24 +0300

    Added the main-layout.hbs file

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,8 @@ router.get('/', (req, res, next) => {
     pageTitle: 'Shop',
     path: '/',
     hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
