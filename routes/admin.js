90278f75e276055df1af98320efce8b946ac9772
manos96n-dev <manos96n@hotmail.com>
2023-08-11 11:38:55 +0300

    Filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-// Middleware function
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post('/product', (req, res) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
