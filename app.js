f3bbe86635de4ce899e5205836b8f636c9b28fe0
manos96n-dev <manos96n@hotmail.com>
2023-08-11 13:32:20 +0300

    Added the css files statically in public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })); // Extracts the data from the form when submit.
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
