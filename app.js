694a6c891f3fa368979f7e1dbcf61d9489989eb1
manos96n-dev <manos96n@hotmail.com>
2023-08-11 11:11:46 +0300

    added the body-parser

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false })); // Extracts the data from the form when submit.
+
 // Middleware function
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post('/product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
