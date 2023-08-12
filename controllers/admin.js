3aacbacb6d28b3db0d0cdb51ed9734537b2b1fa9
manos96n-dev <manos96n@hotmail.com>
2023-08-12 17:19:09 +0300

    Displaying product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,9 +10,9 @@ exports.getAddProduct = (req, res) => {
 exports.postAddProduct = (req, res) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, price, description);
+  const price = req.body.price;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
