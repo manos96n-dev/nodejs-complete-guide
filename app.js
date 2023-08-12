edb2998bdf4141c20c398cd5f36765cabdfb79ce
manos96n-dev <manos96n@hotmail.com>
2023-08-12 12:18:48 +0300

    Added the error.js controller

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs'); // Compile dynamic templates.
 app.set('views', 'views'); // Sets the location of the templates.
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false })); // Extracts the data from the form when submit.
 app.use(express.static(path.join(__dirname, 'public'))); // Sets the public folder for statics files.
@@ -16,8 +17,6 @@ app.use(express.static(path.join(__dirname, 'public'))); // Sets the public fold
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
