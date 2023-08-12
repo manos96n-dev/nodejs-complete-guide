7e6a517eb3acd156765d9d5cb21c15dbfa8c99bb
manos96n-dev <manos96n@hotmail.com>
2023-08-12 22:44:54 +0300

    Added the cart.js model file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res) => {
   Product.fetchAll((products) => {
@@ -37,7 +38,9 @@ exports.getCart = (req, res) => {
 
 exports.postCart = (req, res) => {
   const prodId = req.body.productId;
-  console.log(prodId);
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
 };
 
