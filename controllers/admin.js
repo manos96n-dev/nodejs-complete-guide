cdcacdb085253b69efc013e02cd7f9e5afcbee42
manos96n-dev <manos96n@hotmail.com>
2023-08-13 13:37:19 +0300

    Using query params in /controllers/admin.js file

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,21 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+  });
+};
+
+exports.getEditProduct = (req, res) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  res.render('admin/edit-product', {
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
+    editing: editMode,
   });
 };
 
