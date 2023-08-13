cd85659963e0fa1c013b8398aa71ce7ce0f08213
manos96n-dev <manos96n@hotmail.com>
2023-08-13 15:24:27 +0300

    Done the postEditProduct controller in /controller/admin.js file

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,12 +32,27 @@ exports.postAddProduct = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
-exports.postEditProduct = (req, res) => {};
+exports.postEditProduct = (req, res) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res) => {
   Product.fetchAll((products) => {
