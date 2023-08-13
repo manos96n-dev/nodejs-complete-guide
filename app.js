227fc750f0948cd2a7cb074652d698d133a309e5
manos96n-dev <manos96n@hotmail.com>
2023-08-13 22:04:18 +0300

    Retrieving data from mysql db

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const db = require('./utils/database');
 
 const app = express();
 
 app.set('view engine', 'ejs'); // Compile dynamic templates.
 app.set('views', 'views'); // Sets the location of the templates.
+
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false })); // Extracts the data from the form when submit.
 app.use(express.static(path.join(__dirname, 'public'))); // Sets the public folder for statics files.
--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   database: 'node-complete',
-  password: '123456789',
+  password: '',
 });
 
 module.exports = pool.promise();
