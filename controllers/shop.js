f0e0e6235ac18bcfab0a886253f7c5f2530073f2
manos96n-dev <manos96n@hotmail.com>
2023-08-14 15:09:19 +0300

    Adding products and retrieving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,6 +83,7 @@ exports.getCart = (req, res) => {
 exports.postCart = (req, res) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -94,18 +95,17 @@ exports.postCart = (req, res) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
       res.redirect('/cart');
