41a8d9b7af671188c8deff8d72d3321d4a411bde
manos96n-dev <manos96n@hotmail.com>
2023-08-14 11:40:44 +0300

    Synced the JS definitions with the database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const sequelize = require('./utils/database');
 
 const app = express();
 
@@ -19,4 +20,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
