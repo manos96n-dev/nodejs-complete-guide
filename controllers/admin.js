e089e1e6708be8ad119b725900e246175b4ec81c
manos96n-dev <manos96n@hotmail.com>
2023-08-14 13:20:32 +0300

    Creating a product with an association to the user

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,12 +36,13 @@ exports.postAddProduct = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then((result) => {
       res.redirect('/admin/products');
     })
