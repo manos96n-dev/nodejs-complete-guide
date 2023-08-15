1567e85cce1af63ae7d8f8d67e9c31151784151e
manos96n-dev <manos96n@hotmail.com>
2023-08-15 20:30:09 +0300

    Deleting cart items

--- models/user.js
+++ models/user.js
@@ -64,6 +64,19 @@ class User {
       .catch();
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db.collection('users').findOne({ _id: new ObjectId(userId) });
