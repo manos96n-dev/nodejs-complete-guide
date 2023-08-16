5aa8a936e9c78d0c6054265ebafe9cf0058db64d
manos96n-dev <manos96n@hotmail.com>
2023-08-16 17:49:32 +0300

    Initialized a session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -16,6 +17,9 @@ app.set('views', 'views'); // Sets the location of the templates.
 
 app.use(bodyParser.urlencoded({ extended: false })); // Extracts the data from the form when submit.
 app.use(express.static(path.join(__dirname, 'public'))); // Sets the public folder for statics files.
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false }) // Initialize a session middleware
+);
 
 app.use((req, res, next) => {
   User.findById('64dc9ceddbaf6217b9320f87')
