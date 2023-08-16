8a02681cbf0bf932d3602681872aba6b080f8efc
manos96n-dev <manos96n@hotmail.com>
2023-08-16 21:35:32 +0300

    Implemented a basic signup flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,4 +36,22 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = (req, res) => {};
+exports.postSignup = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => res.redirect('/login'))
+    .catch((err) => console.log(err));
+};
