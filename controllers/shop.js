da1cfbfc2d6639b868f9206458faeeb7c2dc4f71
manos96n-dev <manos96n@hotmail.com>
2023-08-16 13:46:47 +0300

    Deleting cart items with mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,9 +71,8 @@ exports.postCart = (req, res) => {
 
 exports.postCartDeleteProduct = (req, res) => {
   const prodId = req.body.productId;
-
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then((result) => {
       res.redirect('/cart');
     })
