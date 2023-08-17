1057c6de7fdee1087a317eb2f5e67da84f6c30f0
manos96n-dev <manos96n@hotmail.com>
2023-08-17 15:30:37 +0300

    Added async validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -84,7 +84,6 @@ exports.getSignup = (req, res) => {
 exports.postSignup = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -95,32 +94,24 @@ exports.postSignup = (req, res) => {
     });
   }
 
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a diffent one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect('/login');
-          return transporter.sendMail({
-            to: email,
-            from: 'manos1996n@gmail.com',
-            subject: 'Signup succeeded',
-            html: '<h1>You successfully signed up!</h1>',
-          });
-        })
-        .catch((err) => console.log(err));
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
+      return transporter.sendMail({
+        to: email,
+        from: 'manos1996n@gmail.com',
+        subject: 'Signup succeeded',
+        html: '<h1>You successfully signed up!</h1>',
+      });
     })
     .catch((err) => console.log(err));
 };
