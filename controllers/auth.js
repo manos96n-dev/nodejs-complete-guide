8ff6ef86997d297259afa64403976637cccdec33
manos96n-dev <manos96n@hotmail.com>
2023-08-17 18:19:33 +0300

    Added the style in invalid inputs in login

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,6 +26,8 @@ exports.getLogin = (req, res) => {
     path: '/login',
     pageTitle: 'Login',
     errorMessage: message,
+    oldInput: { email: '', password: '' },
+    validationErrors: [],
   });
 };
 
@@ -33,20 +35,27 @@ exports.postLogin = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const errors = validationResult(req);
-
+  console.log(errors.array());
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, password: password },
+      validationErrors: errors.array(),
     });
   }
 
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: { email: email, password: password },
+          validationErrors: [],
+        });
       }
       bcrypt
         .compare(password, user.password)
@@ -59,8 +68,13 @@ exports.postLogin = (req, res) => {
               res.redirect('/');
             });
           }
-          req.flash('error', 'Invalid email or password.');
-          res.redirect('/login');
+          return res.status(422).render('auth/login', {
+            path: '/login',
+            pageTitle: 'Login',
+            errorMessage: 'Invalid email or password.',
+            oldInput: { email: email, password: password },
+            validationErrors: [],
+          });
         })
         .catch((err) => {
           console.log(err);
