a8926674b6c08b859037bde3cef9d2c95134b36d
manos96n-dev <manos96n@hotmail.com>
2023-08-18 10:34:00 +0300

    Sanitized the data which store in db

--- routes/auth.js
+++ routes/auth.js
@@ -11,10 +11,14 @@ router.get('/login', authController.getLogin);
 router.post(
   '/login',
   [
-    check('email').isEmail().withMessage('Please enter a valid email.'),
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .normalizeEmail(),
     body('password', 'Password has to be valid')
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -37,19 +41,23 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       'password',
       'Please enter a password with only numbers and text and at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body('confirmPassword').custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords have to match!');
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      }),
   ],
   authController.postSignup
 );
