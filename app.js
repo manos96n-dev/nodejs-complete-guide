aff20706b16ee5f3d01fbea62b47925842889a7c
manos96n-dev <manos96n@hotmail.com>
2023-08-20 13:44:27 +0300

    Created /feed/posts and /feed/post API endpoints for get and post requests

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+app.use(bodyParser.json()); // Parse incoming json data
 
 app.use('/feed', feedRoutes);
 
