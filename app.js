25d9250eb963c0bfb82a17f8a85edda73b3866e5
manos96n-dev <manos96n@hotmail.com>
2023-08-20 14:04:59 +0300

    Added a general middleware to allows CORS

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(bodyParser.json()); // Parse incoming json data
 
+// This middleware allows the CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
