ea4df8d88cf6303019f6b3b4244b8abfb7ac3bb3
manos96n-dev <manos96n@hotmail.com>
2023-08-20 18:08:14 +0300

    Added server-side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,44 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post.' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post.',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Manos',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
   const title = req.body.title;
   const content = req.body.content;
 
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
+  }
+
   res.status(201).json({
     message: 'Post created successfully!',
-    post: [{ id: new Date().toISOString(), title: title, content: content }],
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: 'Manos',
+      },
+      createdAt: new Date(),
+    },
   });
 };
