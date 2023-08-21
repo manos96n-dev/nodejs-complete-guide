3faf1a4921b607ba04ab55890214bc2dc61a70db
manos96n-dev <manos96n@hotmail.com>
2023-08-21 15:32:47 +0300

    Added authorization on edit and delete of a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -135,6 +135,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -163,6 +168,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post.');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
         throw error;
       }
 
