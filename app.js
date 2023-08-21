f577a887a3cc239a579105fb22ec6dfedf7dec17
manos96n-dev <manos96n@hotmail.com>
2023-08-21 11:05:22 +0300

    Added the User model and auth route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 // Error handling middleware
 app.use((error, req, res, next) => {
