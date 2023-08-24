b1a5d31577a9415e577da1a2c69d25668efe3864
manos96n-dev <manos96n@hotmail.com>
2023-08-24 20:09:33 +0300

    Organized multiple tests

--- test/start.js
+++ test/start.js
@@ -1,8 +1,8 @@
-const expect = require('chai').expect;
+// const expect = require('chai').expect;
 
-it('should add numbers correctly', function () {
-  const num1 = 2;
-  const num2 = 3;
+// it('should add numbers correctly', function () {
+//   const num1 = 2;
+//   const num2 = 3;
 
-  expect(num1 + num2).to.equal(5);
-});
+//   expect(num1 + num2).to.equal(5);
+// });
