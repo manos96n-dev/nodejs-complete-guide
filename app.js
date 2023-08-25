c7bdd09cddeba638cd32ff6dd1242c0af03073ec
manos96n-dev <manos96n@hotmail.com>
2023-08-25 15:56:54 +0300

    Worked with Union types

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    return +num1 + +num2;
 }
-buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    var result2 = add(num1, num2);
     console.log(result);
+    console.log(result2);
 });
