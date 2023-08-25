4b8047d251ac6731bf6d853a44b11f9f6d8324db
manos96n-dev <manos96n@hotmail.com>
2023-08-25 14:34:48 +0300

    Used node core modules with promises

--- response-handler.js
+++ response-handler.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -6,8 +6,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const responseHandler = (req, res, next) => {
-  //   fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //     res.send(data);
-  //   });
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => console.log(err));
+  //   res.sendFile(path.join(__dirname, 'my-page.html'));
 };
