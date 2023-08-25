83d99e91dfa0495ec54022ff593a77a2746506c5
manos96n-dev <manos96n@hotmail.com>
2023-08-25 13:57:29 +0300

    Starting project for modern nodejs module

--- app.js
+++ app.js
@@ -1,68 +1,13 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
+const fs = require('fs');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const express = require('express');
 
 const app = express();
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-app.use(bodyParser.json()); // Parse incoming json data
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
-app.use('/images', express.static(path.join(__dirname, 'images'))); // Static images middleware
-
-// This middleware allows the CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-
-// Error handling middleware
-app.use((error, req, res, next) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, data: data });
-});
-
-mongoose
-  .connect(
-    'mongodb+srv://manos96n:manol123@cluster0.sg2hi.mongodb.net/messages'
-  )
-  .then((result) => {
-    app.listen(8080);
-  })
-  .catch((err) => {
-    console.log(err);
+app.get('/', (req, res, next) => {
+  fs.readFile('my-page.html', 'utf8', (err, data) => {
+    res.send(data);
   });
+});
+
+app.listen(3000);
