d5916d5d3a219356d65b70a54c4f962a30a5cfe6
manos96n-dev <manos96n@hotmail.com>
2023-08-25 16:17:49 +0300

    Worked with generics

--- app.ts
+++ app.ts
@@ -39,3 +39,13 @@ buttonElement.addEventListener('click', () => {
   printResult({ val: result as number, timestamp: new Date() });
   console.log(numResults, textResults);
 });
+
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It worked');
+  }, 1000);
+});
+
+myPromise.then((result) => {
+  console.log(result.split('w'));
+});
