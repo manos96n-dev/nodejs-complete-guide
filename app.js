38e98fa1de17872abfdc88f4aa5c564abaeb7cca
manos96n-dev <manos96n@hotmail.com>
2023-08-25 16:06:05 +0300

    Used Object and Array types

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
+var numResults = [];
+var textResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -10,11 +12,18 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResults.push(result);
     var result2 = add(num1, num2);
+    textResults.push(result2);
     console.log(result);
     console.log(result2);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
