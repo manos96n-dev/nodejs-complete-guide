22a685f21c94478e08a30c022047c84d369a6285
manos96n-dev <manos96n@hotmail.com>
2023-08-26 12:10:12 +0300

    Created a simple web server with deno

--- app.ts
+++ app.ts
@@ -1,8 +1,4 @@
-const text = 'This text should be stored in a file!';
-
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
-
-Deno.writeFile('message.txt', data).then(() => {
-  console.log('Wrote this file!');
-});
+Deno.serve(
+  { port: 3000, hostname: '0.0.0.0' },
+  (_req) => new Response('Hello, world')
+);
